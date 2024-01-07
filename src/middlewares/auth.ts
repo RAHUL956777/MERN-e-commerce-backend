4792db042aa388a077be4c9d4ace1fce43c45146
RAHUL <dasr16583@gmail.com>
2024-01-07 09:46:45 +0530

    working on protected admin route for get all user and delete users

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,17 @@
+import { User } from "../models/user.model";
+import ErrorHandler from "../utils/utilityClass";
+import { TryCatch } from "./error";
+
+export const adminOnly = TryCatch(async (req, res, next) => {
+  const { id } = req.query;
+
+  if (!id) return next(new ErrorHandler("Sale login kar pahle", 401));
+
+  const user = await User.findById(id);
+  if (!user) return next(new ErrorHandler("Sale fake id deta hei", 401));
+
+  if (user.role !== "admin")
+    return next(new ErrorHandler("Sale okat nehi ha teri", 401));
+
+    next();
+});
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,16 +5,17 @@ import {
   getUser,
   newUser,
 } from "../controllers/user.controller";
+import { adminOnly } from "../middlewares/auth";
 
 const app = express.Router();
 
 app.post("/new", newUser);
 
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
 
 // app.get("/:id", getUser);
 // app.get("/:id", deleteUser);  2 routes are same thats why we can chain it
 
-app.route("/:id").get(getUser).delete(deleteUser);
+app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
 
 export default app;
