f3b66f201267fec4122604e1f0b819a270fd4238
RAHUL <dasr16583@gmail.com>
2024-01-07 10:56:37 +0530

    working on product models and configure multer for upload files

---
+++ src/models/product.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter Name"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please enter Photo"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter Number"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter Stock"],
+    },
+    category: {
+      type: String,
+      required: [true, "Please enter product Category"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model("Product", schema);
