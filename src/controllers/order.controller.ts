ad493519d9ede01f91a2d04bf25d5a11eda2d9f0
RAHUL <dasr16583@gmail.com>
2024-01-15 22:11:55 +0530

    working done on my order controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,8 @@ import { TryCatch } from "../middlewares/error";
 import { newOrderRequestBody } from "../types/types";
 import { Order } from "../models/order.model";
 import { invalidateCache, reduceStock } from "../utils/features";
+import ErrorHandler from "../utils/utilityClass";
+import { myCache } from "../app";
 
 export const newOrder = TryCatch(
   async (
@@ -20,6 +22,9 @@ export const newOrder = TryCatch(
       discount,
       total,
     } = req.body;
+
+    if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
+      return next(new ErrorHandler("Please Enter All Fields", 400));
 
     await Order.create({
       shippingInfo,
@@ -42,3 +47,19 @@ export const newOrder = TryCatch(
     });
   }
 );
+
+export const myOrders = TryCatch(async (req, res, next) => {
+  const { id: user } = req.query;
+  const key = `my-orders${user}`;
+  let orders = [];
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find({ user });
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(201).json({
+    success: true,
+    orders,
+  });
+});
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth";
-import { newOrder } from "../controllers/order.controller";
+import {  myOrders, newOrder } from "../controllers/order.controller";
 
 const app = express.Router();
 
 // /api/v1/order/new
 app.post("/new", newOrder);
+
+app.get("/my",myOrders)
 
 export default app;
