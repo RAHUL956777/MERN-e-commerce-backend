75bb6c8349b0e57ba077900c5d148d06da130491
RAHUL <dasr16583@gmail.com>
2024-01-24 23:26:44 +0530

    fix the cors issue

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from "morgan";
 import Stripe from "stripe";
+import cors from "cors";
 
 // importing routes
 import userRoute from "./routes/user.routes";
@@ -32,6 +33,7 @@ export const myCache = new NodeCache();
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("API working with /api/v1");
